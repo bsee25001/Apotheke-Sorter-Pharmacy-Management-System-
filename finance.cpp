3e3e8049de24b3ed0ed3788fe26790550f3378fa
SaifUllah <bsee25001@itu.edu.pk>
2026-05-08 19:06:17 +0500

    GenerateBill_Function_added

--- finance.cpp
+++ finance.cpp
@@ -24,7 +24,159 @@ void menu() {
 
     } while(choice != 0);
 }
+//Function to generateBill
+void generateBill() {
 
+    // READ PRODUCTS.JSON
+    ifstream productFile("products.json");
+
+    if(!productFile) {
+        cout << "products.json file not found!\n";
+        return;
+    }
+
+    json productsData;
+    productFile >> productsData;
+    productFile.close();
+   
+
+cout << "\n========== AVAILABLE PRODUCTS ==========\n";
+
+for(int i = 0; i < productsData["categories"].size(); i++) {
+
+    cout << "\n\nCategory: "
+         << productsData["categories"][i]["category"]
+         << endl;
+
+    for(int j = 0;j < productsData["categories"][i]["products"].size();j++) {
+
+        cout << "ID: "
+             << productsData["categories"][i]["products"][j]["id"]
+
+             << " | Name: "
+             << productsData["categories"][i]["products"][j]["name"]
+
+             << " | Price: "
+             << productsData["categories"][i]["products"][j]["price"]
+
+             << " | Stock: "
+             << productsData["categories"][i]["products"][j]["stock"]
+
+             << endl;
+    }
+}
+
+cout << "\n========================================\n";
+
+    int n;
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    // DMA
+    string *name = new string[n];
+    int *qty = new int[n];
+    float *price = new float[n];
+    float *total = new float[n];
+
+    for(int i = 0; i < n; i++) {
+
+        int productId;
+        bool found = false;
+
+        cout << "\nEnter Product ID: ";
+        cin >> productId;
+
+        cout << "Enter Quantity: ";
+        cin >> qty[i];
+
+        // SEARCH PRODUCT FROM JSON
+        for(auto &category : productsData["categories"]) {
+
+            for(auto &product : category["products"]) {
+
+                if(product["id"] == productId) {
+
+                    found = true;
+
+                    int stock = product["stock"];
+
+                    if(stock < qty[i]) {
+                        cout << "Insufficient stock!\n";
+
+                        delete[] name;
+                        delete[] qty;
+                        delete[] price;
+                        delete[] total;
+
+                        return;
+                    }
+
+                    // READ DATA FROM FILE
+                    name[i] = product["name"];
+                    price[i] = product["price"];
+
+                    total[i] = qty[i] * price[i];
+
+                    // UPDATE STOCK
+                    product["stock"] = stock - qty[i];
+
+                    cout << "\nProduct: " << name[i];
+                    cout << "\nPrice: " << price[i];
+                    cout << "\nRemaining Stock: "
+                         << product["stock"] << endl;
+
+                    break;
+                }
+            }
+        }
+
+        if(!found) {
+            cout << "Product not found!\n";
+
+            delete[] name;
+            delete[] qty;
+            delete[] price;
+            delete[] total;
+
+            return;
+        }
+    }
+
+    // SAVE UPDATED STOCK
+    ofstream updateFile("products.json");
+    updateFile << setw(4) << productsData << endl;
+    updateFile.close();
+
+    float grandTotal = recursiveTotal(total, n);
+
+    grandTotal = applyDiscount(grandTotal);
+    grandTotal = addTax(grandTotal);
+
+    cout << "\n===== FINAL BILL =====\n";
+
+    for(int i = 0; i < n; i++) {
+
+        cout << name[i]
+             << " | Qty: " << qty[i]
+             << " | Price: " << price[i]
+             << " | Total: " << total[i]
+             << endl;
+    }
+
+    cout << "\nBill ID: " << billCounter++;
+    cout << "\nFinal Total: " << grandTotal << endl;
+
+    saveToFile(n, name, qty, price, total, grandTotal);
+
+    // STORE COMPLETE BILL IN bill.json
+    saveJSON(n, name, qty, price, total, grandTotal);
+
+    // FREE MEMORY
+    delete[] name;
+    delete[] qty;
+    delete[] price;
+    delete[] total;
+}
 // NORMAL TOTAL
 float calculateTotal(float *total, int n) {
     
